54255c0a94c51bd63dc049470dd57034e5707537
vv.quang <vv.quang@1pac.vn>
2020-09-25 08:27:49 +0700

    Chang content slider

--- src/components/home/Carousel.vue
+++ src/components/home/Carousel.vue
@@ -12,12 +12,6 @@
     </div>
     <div><h3>1</h3></div>
     <div><h3>2</h3></div>
-    <!--
-    <div><h3>3</h3></div>
-    <div><h3>4</h3></div>
-    <span>5</span>
-    <h2>6</h2>
-    <p>7</p> -->
   </a-carousel>
 </template>
 <script>
